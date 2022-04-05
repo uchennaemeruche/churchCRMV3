0364bc2af3b1645337258acbd0202a53418d6ea1
uchennaemeruche <emerucheucehnna@gmail.com>
2022-04-05 10:46:34 +0100

    Installed aws-s3 upload provider and uploaded config files

--- config/custom.js
+++ config/custom.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  smsApiKey: env("SMS_API_KEY", "192.168.181.12"),
-  smsUrl: env("SMS_URL", "192.168.181.12"),
+  smsApiKey: env("SMS_API_KEY", ""),
+  smsUrl: env("SMS_URL", ""),
   smsUsername: env("SMS_USERNAME", "1uchenna"),
 });
--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 8080),
+  host: env("HOST", "127.0.0.1"),
+  port: env.int("PORT", 1337),
   admin: {
     url: "/dashboard",
     auth: {
