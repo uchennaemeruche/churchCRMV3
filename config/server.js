edf31ae6c3c1f47a4f0888e821fcf543c728090d
uchennaemeruche <emerucheucehnna@gmail.com>
2022-04-05 10:51:17 +0100

    Installed aws-s3 upload provider and uploaded config files

--- config/server.js
+++ config/server.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '127.0.0.1'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "127.0.0.1"),
+  port: env.int("PORT", 1337),
   admin: {
     url: "/dashboard",
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '9409073c14048600fa90118f47d4f3ef'),
+      secret: env("ADMIN_JWT_SECRET", "9409073c14048600fa90118f47d4f3ef"),
     },
   },
-  cron:{
-    enabled:true
-  }
+  cron: {
+    enabled: false,
+  },
 });
