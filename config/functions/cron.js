9fad9ea3539b846f6cd4abde2348123d915c3b2e
uchennaemeruche <emerucheucehnna@gmail.com>
2022-04-06 05:43:27 +0100

    Added resolution for colorjs due to aws deployment error

--- config/functions/cron.js
+++ config/functions/cron.js
@@ -4,7 +4,7 @@
  * Cron config that gives you an opportunity
  * to run scheduled jobs.
  *
- * The cron format consists of:
+ * The cron format consists of: 
  * [SECOND (optional)] [MINUTE] [HOUR] [DAY OF MONTH] [MONTH OF YEAR] [DAY OF WEEK]
  *
  * See more details here: https://strapi.io/documentation/developer-docs/latest/setup-deployment-guides/configurations.html#cron-tasks
