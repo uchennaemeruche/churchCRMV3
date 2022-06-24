c50ef0afabf28757829960b85dd7c9f97ec3f664
Uchenna Emeruche <emerucheuchenna@gmail.com>
2022-06-24 07:11:24 +0100

    add redirect configuration

---
+++ config/middleware.js
@@ -0,0 +1,12 @@
+module.exports = {
+    // load: {
+    //   before: ["timer", "responseTime", "logger", "cors", "responses", "gzip"],
+    //   order: ["parser", ],
+    //   after: ["router", ],
+    // },
+    settings: {
+      redirect: {
+        enabled: true,
+      },
+    },
+  };
